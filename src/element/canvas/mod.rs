1064be61bb4170a63c1156e991a4da446bf94b05
ghostminer <1696415745@qq.com>
2024-01-10 22:59:35 +0800

    # code clean

--- src/element/canvas/mod.rs
+++ src/element/canvas/mod.rs
@@ -3,7 +3,7 @@ mod raw_canvas;
 use cgmath::*;
 use painting::Frame;
 
-use std::{cell::RefCell, rc::Rc};
+use std::sync::{Arc, Mutex};
 
 use web_sys::{HtmlCanvasElement, MouseEvent, PointerEvent};
 use winit::{dpi::PhysicalSize, event_loop::EventLoop, platform::web::EventLoopExtWebSys};
@@ -12,7 +12,10 @@ use self::raw_canvas::RawCanvas;
 
 pub enum Message {
     Init(EventLoop<()>),
-    None,
+    Paingting((f32, f32, f32, PhysicalSize<u32>)),
+    Paint((f32, f32, f32, PhysicalSize<u32>)),
+    EndLine,
+    Nothing,
 }
 
 #[derive(Clone, Debug, yew::Properties, PartialEq)]
@@ -23,7 +26,8 @@ pub struct Props {
 
 pub struct Canvas {
     canvas: yew::NodeRef,
-    p_canvas: Rc<RefCell<Option<RawCanvas>>>,
+    p_canvas: Arc<Mutex<Option<RawCanvas>>>,
+    painting: bool,
 }
 
 impl yew::Component for Canvas {
@@ -33,199 +37,122 @@ impl yew::Component for Canvas {
 
     fn create(_ctx: &yew::Context<Self>) -> Self {
         let canvas = yew::NodeRef::default();
-        let p_canvas = Rc::new(RefCell::new(None));
+        let p_canvas = Arc::new(Mutex::new(None));
 
-        Self { canvas, p_canvas }
+        Self {
+            canvas,
+            p_canvas,
+            painting: false,
+        }
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
-        static mut PAINTING: bool = false;
-
         let props = ctx.props();
 
-        let onmousedown = {
-            let canvas = self.canvas.clone();
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |e: MouseEvent| unsafe {
-                PAINTING = true;
+        let link = ctx.link().clone();
+        let canvas = self.canvas.cast::<HtmlCanvasElement>();
+        let onmousedown = yew::Callback::from(move |e: MouseEvent| {
+            let sz = PhysicalSize::new(
+                canvas.as_ref().unwrap().client_width() as u32,
+                canvas.as_ref().unwrap().client_height() as u32,
+            );
 
-                let sz = PhysicalSize::new(
-                    canvas
-                        .cast::<HtmlCanvasElement>()
-                        .as_ref()
-                        .unwrap()
-                        .client_width() as u32,
-                    canvas
-                        .cast::<HtmlCanvasElement>()
-                        .as_ref()
-                        .unwrap()
-                        .client_height() as u32,
+            let x = e.offset_x() as f32;
+            let y = e.offset_y() as f32;
+
+            let force = {
+                let v = cgmath::Vector2::new(
+                    (e.movement_x() as f32) / (sz.width as f32),
+                    (e.movement_y() as f32) / (sz.height as f32),
                 );
-
-                let force = {
-                    let v = cgmath::Vector2::new(
-                        (e.movement_x() as f32) / (sz.width as f32),
-                        (e.movement_y() as f32) / (sz.height as f32),
-                    );
-                    let force = 1. - v.magnitude() * 100.;
-                    if force > 1. {
-                        1.
-                    } else if force < 0. {
-                        0.
-                    } else {
-                        force
-                    }
-                };
-                let mut binding = p_canvas.borrow_mut();
-                let raw_canvas = binding.as_mut().unwrap();
-                raw_canvas.start_line(raw_canvas.pen.px2point(
-                    e.offset_x() as f32,
-                    e.offset_y() as f32,
-                    force,
-                    sz,
-                ));
-                raw_canvas.window.request_redraw();
-            })
-        };
-
-        let onmouseup = {
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |_| unsafe {
-                PAINTING = false;
-
-                let mut binding = p_canvas.borrow_mut();
-                let raw_canvas = binding.as_mut().unwrap();
-                raw_canvas.end_line();
-                raw_canvas.window.request_redraw();
-            })
-        };
-
-        let onmousemove = {
-            let canvas = self.canvas.clone();
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |e: MouseEvent| unsafe {
-                if PAINTING {
-                    let sz = PhysicalSize::new(
-                        canvas
-                            .cast::<HtmlCanvasElement>()
-                            .as_ref()
-                            .unwrap()
-                            .client_width() as u32,
-                        canvas
-                            .cast::<HtmlCanvasElement>()
-                            .as_ref()
-                            .unwrap()
-                            .client_height() as u32,
-                    );
-
-                    let force = {
-                        let v = cgmath::Vector2::new(
-                            (e.movement_x() as f32) / (sz.width as f32),
-                            (e.movement_y() as f32) / (sz.height as f32),
-                        );
-                        let force = 1. - v.magnitude() * 100.;
-                        if force > 1. {
-                            1.
-                        } else if force < 0. {
-                            0.
-                        } else {
-                            force
-                        }
-                    };
-                    let mut binding = p_canvas.borrow_mut();
-                    let raw_canvas = binding.as_mut().unwrap();
-                    raw_canvas.push_point(raw_canvas.pen.px2point(
-                        e.offset_x() as f32,
-                        e.offset_y() as f32,
-                        force,
-                        sz,
-                    ));
-                    raw_canvas.window.request_redraw();
+                let force = 1. - v.magnitude() * 100.;
+                if force > 1. {
+                    1.
+                } else if force < 0. {
+                    0.
+                } else {
+                    force
                 }
-            })
-        };
+            };
 
-        let onpointerdown = {
-            let canvas = self.canvas.clone();
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |e: PointerEvent| unsafe {
-                PAINTING = true;
+            link.send_message(Message::Paingting((x, y, force, sz)));
+        });
 
-                let sz = PhysicalSize::new(
-                    canvas
-                        .cast::<HtmlCanvasElement>()
-                        .as_ref()
-                        .unwrap()
-                        .client_width() as u32,
-                    canvas
-                        .cast::<HtmlCanvasElement>()
-                        .as_ref()
-                        .unwrap()
-                        .client_height() as u32,
+        let link = ctx.link().clone();
+        let onmouseup = yew::Callback::from(move |_| {
+            link.send_message(Message::EndLine);
+        });
+
+        let link = ctx.link().clone();
+        let canvas = self.canvas.cast::<HtmlCanvasElement>();
+        let onmousemove = yew::Callback::from(move |e: MouseEvent| {
+            let sz = PhysicalSize::new(
+                canvas.as_ref().unwrap().client_width() as u32,
+                canvas.as_ref().unwrap().client_height() as u32,
+            );
+
+            let x = e.offset_x() as f32;
+            let y = e.offset_y() as f32;
+
+            let force = {
+                let v = cgmath::Vector2::new(
+                    (e.movement_x() as f32) / (sz.width as f32),
+                    (e.movement_y() as f32) / (sz.height as f32),
                 );
-
-                let mut binding = p_canvas.borrow_mut();
-                let raw_canvas = binding.as_mut().unwrap();
-                raw_canvas.start_line(raw_canvas.pen.px2point(
-                    e.offset_x() as f32,
-                    e.offset_y() as f32,
-                    if e.pointer_type() == "touch" {
-                        e.pressure() / 10.
-                    } else {
-                        e.pressure()
-                    },
-                    sz,
-                ));
-                raw_canvas.window.request_redraw();
-            })
-        };
-
-        let onpointerup = {
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |_| unsafe {
-                PAINTING = false;
-
-                let mut binding = p_canvas.borrow_mut();
-                let raw_canvas = binding.as_mut().unwrap();
-                raw_canvas.end_line();
-                raw_canvas.window.request_redraw();
-            })
-        };
-
-        let onpointermove = {
-            let canvas = self.canvas.clone();
-            let p_canvas = self.p_canvas.clone();
-            yew::Callback::from(move |e: PointerEvent| unsafe {
-                if PAINTING {
-                    let sz = PhysicalSize::new(
-                        canvas
-                            .cast::<HtmlCanvasElement>()
-                            .as_ref()
-                            .unwrap()
-                            .client_width() as u32,
-                        canvas
-                            .cast::<HtmlCanvasElement>()
-                            .as_ref()
-                            .unwrap()
-                            .client_height() as u32,
-                    );
-
-                    let mut binding = p_canvas.borrow_mut();
-                    let raw_canvas = binding.as_mut().unwrap();
-                    raw_canvas.push_point(raw_canvas.pen.px2point(
-                        e.offset_x() as f32,
-                        e.offset_y() as f32,
-                        if e.pointer_type() == "touch" {
-                            e.pressure() / 10.
-                        } else {
-                            e.pressure()
-                        },
-                        sz,
-                    ));
-                    raw_canvas.window.request_redraw();
+                let force = 1. - v.magnitude() * 100.;
+                if force > 1. {
+                    1.
+                } else if force < 0. {
+                    0.
+                } else {
+                    force
                 }
-            })
-        };
+            };
+            link.send_message(Message::Paint((x, y, force, sz)));
+        });
+
+        let link = ctx.link().clone();
+        let canvas = self.canvas.cast::<HtmlCanvasElement>();
+        let onpointerdown = yew::Callback::from(move |e: PointerEvent| {
+            let sz = PhysicalSize::new(
+                canvas.as_ref().unwrap().client_width() as u32,
+                canvas.as_ref().unwrap().client_height() as u32,
+            );
+
+            let x = e.offset_x() as f32;
+            let y = e.offset_y() as f32;
+
+            let force = if e.pointer_type() == "touch" {
+                e.pressure() / 10.
+            } else {
+                e.pressure()
+            };
+            link.send_message(Message::Paint((x, y, force, sz)));
+        });
+
+        let link = ctx.link().clone();
+        let onpointerup = yew::Callback::from(move |_| {
+            link.send_message(Message::EndLine);
+        });
+
+        let link = ctx.link().clone();
+        let canvas = self.canvas.cast::<HtmlCanvasElement>();
+        let onpointermove = yew::Callback::from(move |e: PointerEvent| {
+            let sz = PhysicalSize::new(
+                canvas.as_ref().unwrap().client_width() as u32,
+                canvas.as_ref().unwrap().client_height() as u32,
+            );
+
+            let x = e.offset_x() as f32;
+            let y = e.offset_y() as f32;
+
+            let force = if e.pointer_type() == "touch" {
+                e.pressure() / 10.
+            } else {
+                e.pressure()
+            };
+            link.send_message(Message::Paint((x, y, force, sz)));
+        });
 
         yew::html! {
             <canvas ref={self.canvas.clone()} class={props.classes.clone()}
@@ -241,40 +168,69 @@ impl yew::Component for Canvas {
 
     fn update(&mut self, _ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Self::Message::Init(event_loop) => {
+            Message::Init(event_loop) => {
                 let canvas = self.canvas.clone();
                 let p_canvas = self.p_canvas.clone();
                 event_loop.spawn(move |event, target, control_flow| {
-                    let mut binding = p_canvas.borrow_mut();
-                    let raw_canvas = binding.as_mut().unwrap();
+                    let mut op = p_canvas.lock().unwrap();
+                    let raw_canvas = op.as_mut().unwrap();
                     raw_canvas.on_event(canvas.clone(), event, target, control_flow);
                 });
                 true
             }
-            Message::None => false,
+            Message::Nothing => false,
+            Message::Paingting((x, y, force, sz)) => {
+                self.painting = true;
+                let mut op = self.p_canvas.lock().unwrap();
+                let raw_canvas = op.as_mut().unwrap();
+                raw_canvas.start_line(raw_canvas.pen.px2point(x, y, force, sz));
+                raw_canvas.window.request_redraw();
+                false
+            }
+            Message::EndLine => {
+                self.painting = false;
+                let mut op = self.p_canvas.lock().unwrap();
+                let raw_canvas = op.as_mut().unwrap();
+                raw_canvas.end_line();
+                raw_canvas.window.request_redraw();
+                false
+            }
+            Message::Paint((x, y, force, sz)) => {
+                if self.painting {
+                    let mut op = self.p_canvas.lock().unwrap();
+                    let raw_canvas = op.as_mut().unwrap();
+                    raw_canvas.push_point(raw_canvas.pen.px2point(x, y, force, sz));
+                    raw_canvas.window.request_redraw();
+                }
+                false
+            }
         }
     }
 
     fn rendered(&mut self, ctx: &yew::Context<Self>, first_render: bool) {
-        if first_render {
-            if self.p_canvas.borrow().is_some() {
-                return;
-            }
-
-            let canvas = self.canvas.clone();
-            let p_canvas = self.p_canvas.clone();
-            ctx.link().send_future(async move {
-                let event_loop = EventLoop::new();
-                let raw_canvas = match RawCanvas::create(canvas.clone(), &event_loop).await {
-                    Ok(o) => o,
-                    Err(e) => {
-                        log::error!("failed to build raw_canvas: {:?}", e);
-                        return Self::Message::None;
-                    }
-                };
-                p_canvas.replace(Some(raw_canvas));
-                Self::Message::Init(event_loop)
-            });
+        if !first_render {
+            return;
         }
+        let p_canvas = self.p_canvas.lock().unwrap();
+        if p_canvas.is_some() {
+            return;
+        }
+        drop(p_canvas);
+
+        let canvas = self.canvas.clone();
+        let p_canvas = self.p_canvas.clone();
+        ctx.link().send_future(async move {
+            let event_loop = EventLoop::new();
+            let raw_canvas = match RawCanvas::create(canvas.clone(), &event_loop).await {
+                Ok(o) => o,
+                Err(e) => {
+                    log::error!("failed to build raw_canvas: {:?}", e);
+                    return Message::Nothing;
+                }
+            };
+            let mut p_canvas = p_canvas.lock().unwrap();
+            *p_canvas = Some(raw_canvas);
+            Message::Init(event_loop)
+        });
     }
 }
